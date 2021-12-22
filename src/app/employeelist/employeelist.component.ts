f4fa8070323fb489ddc2d4de506a59bc8689b837
Abhishek verma <94790495+abhithinks@users.noreply.github.com>
2021-12-22 20:59:19 +0530

    Update employeelist.component.ts

--- src/app/employeelist/employeelist.component.ts
+++ src/app/employeelist/employeelist.component.ts
@@ -1,26 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 
 @Component({
@@ -29,8 +7,11 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./employeelist.component.css']
 })
 export class EmployeelistComponent implements OnInit {
-  
-
+  showMe:boolean=false;
+  showMe1:boolean=false;
+  showMe2:boolean=false;
+  showMe3:boolean=false;
+  showMe4:boolean=false;
   constructor() { }
   
   
@@ -38,10 +19,23 @@ export class EmployeelistComponent implements OnInit {
   ngOnInit(): void {
     
   }
-  displayedColumns: string[] = ['demo-position', 'demo-name', 'demo-weight', 'demo-symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+ 
+toggleTag(){
+  this.showMe=!this.showMe;
+}
+toggleTag1(){
+  this.showMe1=!this.showMe1;
+}
+toggleTag2(){
+  this.showMe2=!this.showMe2;
+}
 
+toggleTag3(){
+  this.showMe3=!this.showMe3;
+}
 
-
+toggleTag4(){
+  this.showMe4=!this.showMe4;
+}
 
 }
